7e305df2814c28f83a548437d8c1e6e6164ee629
Go Erik <erkgo.eg@gmail.com>
2024-01-14 23:29:48 +0800

    changed shell to powershell

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -1,1 +1,7 @@
 require("custom.remap")
+
+-- Changes the shell from Command Prompt to Powershell only if nvim instance is in Windows
+if vim.loop.os_uname().sysname ~= 'Windows_NT' then return end
+vim.opt.shell = 'powershell'
+vim.opt.shellcmdflag = '-nologo -noprofile -ExecutionPolicy RemoteSigned -command'
+vim.opt.shellxquote = ''
