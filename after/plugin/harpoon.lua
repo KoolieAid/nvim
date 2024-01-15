ccc8efe82daac28bd540ced1c39c2f9ab717162b
Go Erik <erkgo.eg@gmail.com>
2024-01-15 22:04:06 +0800

    harpoon stuff

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -1,10 +1,13 @@
 local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
 
-vim.keymap.set("n", "<leader>a", mark.add_file)
-vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+vim.keymap.set('n', '<leader>n', ui.nav_next)
+vim.keymap.set('n', '<leader>p', ui.nav_prev)
 
--- vim.keymap.set("n", "<leader>r", mark.remove_file)
+vim.keymap.set("n", "<leader>a", mark.add_file)
+vim.keymap.set("n", "<leader>r", mark.rm_file)
+
+vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
 
 vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
 vim.keymap.set("n", "<C-t>", function() ui.nav_file(2) end)
