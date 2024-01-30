dd70a74f0bdff51fdf9e7ca394625f9be9a7bc08
Go Erik <erkgo.eg@gmail.com>
2024-01-31 03:49:57 +0800

    added neovide support

---
+++ lua/custom/neovide.lua
@@ -0,0 +1,12 @@
+vim.o.guifont = "JetBrainsMono Nerd Font Mono"
+
+vim.g.neovide_floating_z_height = 10
+vim.g.neovide_light_angle_degrees = 10
+vim.g.neovide_light_radius = 10
+
+vim.g.neovide_hide_mouse_when_typing = true
+
+vim.g.neovide_window_blurred = true
+vim.g.neovide_floating_blur_amount_y = -1
+
+vim.g.neovide_cursor_vfx_mode = "torpedo"
