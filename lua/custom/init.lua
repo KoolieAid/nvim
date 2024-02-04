5857309f00af8743cc031ab5917be0edbff3d143
Go Erik <erkgo.eg@gmail.com>
2024-02-04 23:06:56 +0800

    reordered windows checker, now branches properly

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -2,12 +2,11 @@ require("custom.remap")
 require("custom.lazy")
 
 -- Changes the shell from Command Prompt to Powershell only if nvim instance is in Windows
-if vim.loop.os_uname().sysname ~= "Windows_NT" then
-    return
+if vim.loop.os_uname().sysname == "Windows_NT" then
+    vim.opt.shell = "powershell"
+    vim.opt.shellcmdflag = "-nologo -noprofile -ExecutionPolicy RemoteSigned -command"
+    vim.opt.shellxquote = ""
 end
-vim.opt.shell = "powershell"
-vim.opt.shellcmdflag = "-nologo -noprofile -ExecutionPolicy RemoteSigned -command"
-vim.opt.shellxquote = ""
 
 if vim.g.neovide then
     require("custom.neovide")
