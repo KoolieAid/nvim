76b7cad78cdbbff0bbde6db8107d0ca63fd9c118
Go Erik <erkgo.eg@gmail.com>
2024-02-04 12:20:31 +0800

    added git signs

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,6 +1,13 @@
 return {
-    "tpope/vim-fugitive",
-    config = function()
-        vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
-    end,
+    {
+        "tpope/vim-fugitive",
+        config = function()
+            vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+        end,
+    },
+    {
+        "lewis6991/gitsigns.nvim",
+        event = "InsertEnter",
+        config = true,
+    },
 }
