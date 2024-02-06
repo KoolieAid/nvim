619799c381713385ab9841ce4cfea0c9f2f49909
Erik Go <erkgo.eg@gmail.com>
2024-02-06 21:45:34 +0800

    added new system remaps and <C-c>

--- lua/custom/remap.lua
+++ lua/custom/remap.lua
@@ -10,10 +10,17 @@ vim.keymap.set("n", "N", "Nzzzv")
 -- Delete to void and paste. (In Visual Block)
 vim.keymap.set("x", "<leader>p", '"_dP')
 
+-- System copy paste
 vim.keymap.set("n", "<leader>y", '"+y')
 vim.keymap.set("v", "<leader>y", '"+y')
 vim.keymap.set("n", "<leader>Y", '"+Y')
 
+vim.keymap.set("n", "<leader>sp", '"+p')
+vim.keymap.set("n", "<leader>sP", '"+P')
+
 -- Visual Move by Primeagen
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Overide Ctrl-c with Esc
+vim.keymap.set({ "n", "i", "v", "c" }, "<C-c>", "<Esc>")
