bce6433b248556af83096856bc1d81e73b2aa57d
Go Erik <erkgo.eg@gmail.com>
2024-02-10 21:52:32 +0800

    added noice and tree-sitter context

--- lua/plugins/tree-sitter.lua
+++ lua/plugins/tree-sitter.lua
@@ -1,44 +1,52 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    event = "InsertEnter",
-    enabled = function()
-        local compilers = { "gcc", "clang", "cc" }
-        for _, c in ipairs(compilers) do
-            local out = os.execute(c .. " --version")
-            if out == 0 then
-                return true
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        event = "InsertEnter",
+        enabled = function()
+            local compilers = { "gcc", "clang", "cc" }
+            for _, c in ipairs(compilers) do
+                local out = os.execute(c .. " --version")
+                if out == 0 then
+                    return true
+                end
             end
-        end
-        return false
-    end,
-    config = function()
-        require("nvim-treesitter.configs").setup({
-            -- A list of parser names, or "all" (the five listed parsers should always be installed)
-            ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "c_sharp" },
+            return false
+        end,
+        config = function()
+            require("nvim-treesitter.configs").setup({
+                -- A list of parser names, or "all" (the five listed parsers should always be installed)
+                ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "c_sharp" },
 
-            -- Install parsers synchronously (only applied to `ensure_installed`)
-            sync_install = false,
+                -- Install parsers synchronously (only applied to `ensure_installed`)
+                sync_install = false,
 
-            -- Automatically install missing parsers when entering buffer
-            -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-            auto_install = true,
+                -- Automatically install missing parsers when entering buffer
+                -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+                auto_install = true,
 
-            -- List of parsers to ignore installing (or "all")
-            ignore_install = { "javascript" },
+                -- List of parsers to ignore installing (or "all")
+                ignore_install = { "javascript" },
 
-            ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-            -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+                ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
+                -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
 
-            highlight = {
-                enable = true,
+                highlight = {
+                    enable = true,
 
-                -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-                -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-                -- Using this option may slow down your editor, and you may see some duplicate highlights.
-                -- Instead of true it can also be a list of languages
-                additional_vim_regex_highlighting = false,
-            },
-        })
-    end,
+                    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+                    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+                    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+                    -- Instead of true it can also be a list of languages
+                    additional_vim_regex_highlighting = false,
+                },
+            })
+        end,
+    },
+    {
+        "nvim-treesitter/nvim-treesitter-context",
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter",
+        },
+    },
 }
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,49 +1,58 @@
 return {
-	"nvim-tree/nvim-web-devicons",
-	{
-		"goolord/alpha-nvim",
-		dependencies = {
-			"nvim-tree/nvim-web-devicons",
-		},
-		config = function()
-			local alpha = require("alpha")
-			local dashboard = require("alpha.themes.dashboard")
+    "nvim-tree/nvim-web-devicons",
+    {
+        "goolord/alpha-nvim",
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
+        },
+        config = function()
+            local alpha = require("alpha")
+            local dashboard = require("alpha.themes.dashboard")
 
-			dashboard.section.header.val = {
-				[[                                                                       ]],
-				[[                                                                       ]],
-				[[                                                                       ]],
-				[[                                                                       ]],
-				[[                                                                     ]],
-				[[       ████ ██████           █████      ██                     ]],
-				[[      ███████████             █████                             ]],
-				[[      █████████ ███████████████████ ███   ███████████   ]],
-				[[     █████████  ███    █████████████ █████ ██████████████   ]],
-				[[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
-				[[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
-				[[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
-				[[                                                                       ]],
-				[[                                                                       ]],
-				[[                                                                       ]],
-			}
+            dashboard.section.header.val = {
+                [[                                                                       ]],
+                [[                                                                       ]],
+                [[                                                                       ]],
+                [[                                                                       ]],
+                [[                                                                     ]],
+                [[       ████ ██████           █████      ██                     ]],
+                [[      ███████████             █████                             ]],
+                [[      █████████ ███████████████████ ███   ███████████   ]],
+                [[     █████████  ███    █████████████ █████ ██████████████   ]],
+                [[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
+                [[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
+                [[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
+                [[                                                                       ]],
+                [[                                                                       ]],
+                [[                                                                       ]],
+            }
 
-			alpha.setup(dashboard.opts)
-		end,
-	},
-	-- Status line
-	{
-		"nvim-lualine/lualine.nvim",
-		dependencies = {
-			"nvim-tree/nvim-web-devicons",
-		},
-		config = true,
-	},
-	{
-		"nanozuki/tabby.nvim",
-		config = function()
-			require("tabby.tabline").use_preset("active_wins_at_tail", {
-				lualine_theme = "auto",
-			})
-		end,
-	},
+            alpha.setup(dashboard.opts)
+        end,
+    },
+    -- Status line
+    {
+        "nvim-lualine/lualine.nvim",
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
+        },
+        config = true,
+    },
+    {
+        "nanozuki/tabby.nvim",
+        config = function()
+            require("tabby.tabline").use_preset("active_wins_at_tail", {
+                lualine_theme = "auto",
+            })
+        end,
+    },
+    {
+        "folke/noice.nvim",
+        event = "VeryLazy",
+        config = true,
+        dependencies = {
+            "MunifTanjim/nui.nvim",
+            "rcarriga/nvim-notify",
+        },
+    },
 }
