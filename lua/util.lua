df29d6c34496de039331c832540ec57ed7ec3028
Go Erik <erkgo.eg@gmail.com>
2024-02-11 11:30:38 +0800

    use built in api instead of creating new one

--- lua/util.lua
+++ lua/util.lua
@@ -1,10 +1,1 @@
-function table.deepcopy(og)
-    local cpy = {}
-    for k, v in pairs(og) do
-        if type(v) == "table" then
-            v = table.deepcopy(v)
-        end
-        cpy[k] = v
-    end
-    return cpy
-end
+table.deepcopy = vim.deepcopy
