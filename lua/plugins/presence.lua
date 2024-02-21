b7dc8fdaa0fda52d3911ba0635fdae5410e1fd07
Go Erik <erkgo.eg@gmail.com>
2024-02-22 01:50:35 +0800

    disabled presence due to crashes on multiple instances of neovim

--- lua/plugins/presence.lua
+++ lua/plugins/presence.lua
@@ -1,5 +1,7 @@
 return {
+    -- Disabled due to crashes on multiple instances of Neovim
 	"andweeb/presence.nvim",
+    enabled = false,
     event = "VeryLazy",
 	opts = {
 		neovim_image_text = "neovim btw",
