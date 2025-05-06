e77d1d20ad8ffd5e23474aa2b4ae2cc91c311f29
unknown <erkgo.eg@gmail.com>
2025-05-06 23:56:23 +0800

    feat(fun): Added syntax-gaslighting for funsies

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -29,4 +29,9 @@ return {
         config = true,
         cmd = "Rooter",
     },
+    {
+        "NotAShelf/syntax-gaslighting.nvim",
+        event = { "BufRead", "BufAdd" },
+        config = true,
+    },
 }
