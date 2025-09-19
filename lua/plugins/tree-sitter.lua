e74112e3a0ac33c82a9f168e4d22256bb166229c
Go Erik <erkgo.eg@gmail.com>
2025-09-19 23:20:54 +0800

    feat(ts)!: Changed branch to main

--- lua/plugins/tree-sitter.lua
+++ lua/plugins/tree-sitter.lua
@@ -1,50 +1,48 @@
-local ts_enabled = true
-
 return {
     {
         "nvim-treesitter/nvim-treesitter",
-        enabled = ts_enabled,
+        branch = "main",
+        lazy = false,
         build = ":TSUpdate",
         event = "InsertEnter",
-        config = function()
-
-            -- If on a Windows machine use zig compiler, since it works better on Windows
-            if vim.loop.os_uname().sysname == "Windows_NT" then
-                require("nvim-treesitter.install").compilers = { "zig" }
-            end
-
-            require("nvim-treesitter.configs").setup({
-                -- A list of parser names, or "all" (the five listed parsers should always be installed)
-                ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "c_sharp" },
-
-                -- Install parsers synchronously (only applied to `ensure_installed`)
-                sync_install = false,
-
-                -- Automatically install missing parsers when entering buffer
-                -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-                auto_install = true,
-
-                -- List of parsers to ignore installing (or "all")
-                ignore_install = { "javascript" },
-
-                ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-                -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
-
-                highlight = {
-                    enable = true,
-
-                    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-                    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-                    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-                    -- Instead of true it can also be a list of languages
-                    additional_vim_regex_highlighting = false,
-                },
-            })
-        end,
+        -- config = function()
+        --
+        --     -- If on a Windows machine use zig compiler, since it works better on Windows
+        --     if vim.loop.os_uname().sysname == "Windows_NT" then
+        --         require("nvim-treesitter.install").compilers = { "zig" }
+        --     end
+        --
+        --     require("nvim-treesitter.configs").setup({
+        --         -- A list of parser names, or "all" (the five listed parsers should always be installed)
+        --         ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "c_sharp" },
+        --
+        --         -- Install parsers synchronously (only applied to `ensure_installed`)
+        --         sync_install = false,
+        --
+        --         -- Automatically install missing parsers when entering buffer
+        --         -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+        --         auto_install = true,
+        --
+        --         -- List of parsers to ignore installing (or "all")
+        --         ignore_install = { "javascript" },
+        --
+        --         ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
+        --         -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+        --
+        --         highlight = {
+        --             enable = true,
+        --
+        --             -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+        --             -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+        --             -- Using this option may slow down your editor, and you may see some duplicate highlights.
+        --             -- Instead of true it can also be a list of languages
+        --             additional_vim_regex_highlighting = false,
+        --         },
+        --     })
+        -- end,
     },
     {
         "nvim-treesitter/nvim-treesitter-context",
-        enabled = ts_enabled,
         event = "LspAttach",
         dependencies = {
             "nvim-treesitter/nvim-treesitter",
@@ -52,27 +50,66 @@ return {
     },
     {
         "nvim-treesitter/nvim-treesitter-textobjects",
-        enabled = ts_enabled,
+        branch = "main",
         event = "LspAttach",
         config = function ()
-            require("nvim-treesitter.configs").setup({
-                textobjects = {
-                    select = {
-                        enable = true,
-
-                        -- Automatically jump forward to textobj, similar to targets.vim
-                        lookahead = true,
-
-                        keymaps = {
-                            -- You can use the capture groups defined in textobjects.scm
-                            ["af"] = "@function.outer",
-                            ["if"] = "@function.inner",
-                            -- ["ac"] = "@class.outer",
-                            -- ["ic"] = "@class.inner",
-                        },
-                    },
+            require("nvim-treesitter-textobjects").setup {
+                select = {
+                    -- Automatically jump forward to textobj, similar to targets.vim
+                    lookahead = true,
+                    -- You can choose the select mode (default is charwise 'v')
+                    --
+                    -- Can also be a function which gets passed a table with the keys
+                    -- * query_string: eg '@function.inner'
+                    -- * method: eg 'v' or 'o'
+                    -- and should return the mode ('v', 'V', or '<c-v>') or a table
+                    -- mapping query_strings to modes.
+                    -- selection_modes = {
+                    --     ['@parameter.outer'] = 'v', -- charwise
+                    --     ['@function.outer'] = 'V', -- linewise
+                    --     ['@class.outer'] = '<c-v>', -- blockwise
+                    -- },
+                    -- If you set this to `true` (default is `false`) then any textobject is
+                    -- extended to include preceding or succeeding whitespace. Succeeding
+                    -- whitespace has priority in order to act similarly to eg the built-in
+                    -- `ap`.
+                    --
+                    -- Can also be a function which gets passed a table with the keys
+                    -- * query_string: eg '@function.inner'
+                    -- * selection_mode: eg 'v'
+                    -- and should return true of false
+                    include_surrounding_whitespace = false,
                 },
-            })
+            }
+
+            -- keymaps
+            -- You can use the capture groups defined in `textobjects.scm`
+            vim.keymap.set({ "x", "o" }, "af", function()
+                require "nvim-treesitter-textobjects.select".select_textobject("@function.outer", "textobjects")
+            end)
+            vim.keymap.set({ "x", "o" }, "if", function()
+                require "nvim-treesitter-textobjects.select".select_textobject("@function.inner", "textobjects")
+            end)
+
+            -- 'master' version of treesitter
+            -- require("nvim-treesitter-textobjects").setup({
+            --     textobjects = {
+            --         select = {
+            --             enable = true,
+            --
+            --             -- Automatically jump forward to textobj, similar to targets.vim
+            --             lookahead = true,
+            --
+            --             keymaps = {
+            --                 -- You can use the capture groups defined in textobjects.scm
+            --                 ["af"] = "@function.outer",
+            --                 ["if"] = "@function.inner",
+            --                 -- ["ac"] = "@class.outer",
+            --                 -- ["ic"] = "@class.inner",
+            --             },
+            --         },
+            --     },
+            -- })
         end,
     },
 }
